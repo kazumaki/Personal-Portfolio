c379994feca17362d90ea0666064b879c51ba237
kazumaki <vcampos.pitangui@gmail.com>
2020-06-21 17:44:11 -0300

    Updates

---
+++ precache-manifest.cf1b2b8f8639420ee6e491265ec96bdb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b10848eba7a459271686f5ce354e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bc58670a42a2fa0178e",
+    "url": "/static/css/main.d8a458d3.chunk.css"
+  },
+  {
+    "revision": "0e9129195e59f413d3bc",
+    "url": "/static/js/2.2b3815f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b3815f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc58670a42a2fa0178e",
+    "url": "/static/js/main.05de25ef.chunk.js"
+  },
+  {
+    "revision": "4d030bf2aa0c41b6f6cf",
+    "url": "/static/js/runtime-main.ccef853a.js"
+  },
+  {
+    "revision": "9981b95f9300f5a723b550130b2c0fd7",
+    "url": "/static/media/billy-the-jumper-mock-shadow.9981b95f.png"
+  },
+  {
+    "revision": "f444ca8caec8704661c7c0b01bf54e52",
+    "url": "/static/media/facebook-clone-mock-shadow.f444ca8c.png"
+  },
+  {
+    "revision": "cf2c093af0409b73432fe44209f235ef",
+    "url": "/static/media/lol-champion-db-mock-shadow.cf2c093a.png"
+  }
+]);
