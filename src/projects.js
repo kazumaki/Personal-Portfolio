5baad4ebbb6a0b7478d31dca1e239da75601c9dc
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-06-21 18:05:06 -0300

    Fix linter errors

--- src/projects.js
+++ src/projects.js
@@ -1,10 +1,14 @@
+const facebookImage = require('./images/facebook-clone-mock-shadow.png');
+const billyImage = require('./images/billy-the-jumper-mock-shadow.png');
+const lolImage = require('./images/lol-champion-db-mock-shadow.png');
+
 const projects = [
   {
     name: 'Facebook Clone',
     description: 'A Facebook-like Social network built in Ruby on Rails',
     link: 'https://github.com/kazumaki/facebook-clone',
     icon: 'fa-github-square',
-    img: require('./images/facebook-clone-mock-shadow.png'),
+    img: facebookImage,
     background: '#fe4a49',
   },
   {
@@ -12,7 +16,7 @@ const projects = [
     description: 'A simple game made with JavaScript',
     link: 'https://www.github.com/kazumaki/billy-the-jumper',
     icon: 'fa-github-square',
-    img: require('./images/billy-the-jumper-mock-shadow.png'),
+    img: billyImage,
     background: '#2ab7ca',
   },
   {
@@ -20,7 +24,7 @@ const projects = [
     description: 'A SPA made with async React & Redux that fetches Riot Games public API',
     link: 'https://www.github.com/kazumaki',
     icon: 'fa-github-square',
-    img: require('./images/lol-champion-db-mock-shadow.png'),
+    img: lolImage,
     background: '#fed766',
   },
 ];
